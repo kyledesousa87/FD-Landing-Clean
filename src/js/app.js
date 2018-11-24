991161ccae1fbb4bff2028ab466aa7dbd0409bcb
Kyle De Sousa <kyledesousa@finestdental.co.uk>
2018-11-24 14:31:52 +0000

    Added booking form

--- src/js/app.js
+++ src/js/app.js
@@ -284,11 +284,11 @@ $(document).ready(function() {
                 nav:true
             },
             600:{
-                items:3,
+                items:2,
                 nav:false
             },
             1000:{
-                items:3,
+                items:2,
                 nav:true,
                 loop:false
             }
